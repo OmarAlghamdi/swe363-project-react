9499b7c555a1da0a4dcb8f8ad51d23bf4786767e
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-11-17 11:32:11 +0300

    adds the initital data schema and dummy datastore these are added to ease the development of the application.
    no data will be stored on the fornt end. these are temporary until the
    the backend is ready

---
+++ data-schema.js
@@ -0,0 +1,30 @@
+const users = [
+    {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    type: ''
+    }
+]
+
+const events = [
+    {
+        name: '',
+        desc: '',
+        startDate: '',
+        startTime: '',
+        endDate: '',
+        endTime: '',
+        creator: '',
+        joined: [],
+        waiting: [],
+        feedbacks: [
+            {
+                by: '',
+                content: '',
+                reply: ''
+            }
+        ]
+    }
+]
---
+++ datastore.js
@@ -0,0 +1,75 @@
+const users = [
+    {
+    firstName: 'Omar',
+    lastName: 'Alghamdi',
+    email: 'omar@kfupm.com',
+    password: 'omar123',
+    type: 'user'
+    },
+    {
+        firstName: 'Abdullah',
+        lastName: 'Khayat',
+        email: 'abdullah@kfupm.com',
+        password: 'abdul123',
+        type: 'user'
+    },
+    {
+        firstName: 'Mohammed',
+        lastName: 'Alattas',
+        email: 'mohd@kfupm.com',
+        password: 'mohd123',
+        type: 'user'
+    },
+    {
+        firstName: 'Hamza',
+        lastName: 'Luqman',
+        email: 'hamza@kfupm.com',
+        password: 'hamza123',
+        type: 'admin'
+    }
+]
+
+const events = [
+    {
+        name: 'football',
+        desc: 'footbal in field 5',
+        startDate: '20/11/2019',
+        startTime: '07:00pm',
+        endDate: '20/11/2019',
+        endTime: '09:00pm',
+        creator: 'omar@kfupm.com',
+        joined: [],
+        waiting: ['abdul@kfupm.com', 'mohd@kfupm.com'],
+        feedbacks: []
+    },
+    {
+        name: 'baskerball',
+        desc: 'basketball in building 11',
+        startDate: '21/11/2019',
+        startTime: '07:00pm',
+        endDate: '21/11/2019',
+        endTime: '09:00pm',
+        creator: 'abdul@kfupm.com',
+        joined: ['omar@kfupm.com'],
+        waiting: ['mohd@kfupm.com'],
+        feedbacks: []
+    },
+    {
+        name: 'football',
+        desc: 'footbal in field 5',
+        startDate: '22/11/2019',
+        startTime: '07:00pm',
+        endDate: '22/11/2019',
+        endTime: '09:00pm',
+        creator: 'modh@kfupm.com',
+        joined: ['abdul@kfupm.com', 'omar@kfupm.com'],
+        waiting: [],
+        feedbacks: [
+            {
+                by: 'omar@kfupm.com',
+                content: 'great match',
+                reply: ''
+            }
+        ]
+    },
+]
