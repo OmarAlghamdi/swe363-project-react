b120c672c8ff33b1a494a5396775692ebbb7432f
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-11-18 17:31:44 +0300

    creates a new signin page it looks the same as the orinial but this is class based

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
-import SignIn from './components/Signin';
-import SignUp from './components/Signup';
-import History from './components/History';
-import Feedback from './components/Feedback';
-import NewEvent from './components/NewEvent';
-import Profile from './components/Profile';
-import Users from './components/Users';
+import SignIn from './components/Signin-c'
+import SignUp from './components/Signup'
+import History from './components/History'
+import Feedback from './components/Feedback'
+import NewEvent from './components/NewEvent'
+import Profile from './components/Profile'
+import Users from './components/Users'
 
 import { users as userList, events as eventList } from './datastore'
 
