9c080f49e10afa424f5668417998bc829069b71c
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-11-19 21:10:47 +0300

    Updates

---
+++ precache-manifest.4f42066b44472cda336763d59aa7564d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93321c21217ef1baf7b29324eea71e1",
+    "url": "/swe363-project-react/index.html"
+  },
+  {
+    "revision": "effbc35eb6c9d2babc79",
+    "url": "/swe363-project-react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "168197bb2528ef5a9afe",
+    "url": "/swe363-project-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "effbc35eb6c9d2babc79",
+    "url": "/swe363-project-react/static/js/2.fc94f13e.chunk.js"
+  },
+  {
+    "revision": "168197bb2528ef5a9afe",
+    "url": "/swe363-project-react/static/js/main.f00f670c.chunk.js"
+  },
+  {
+    "revision": "1f9a77fb66cf91e0cd12",
+    "url": "/swe363-project-react/static/js/runtime-main.7f07098b.js"
+  },
+  {
+    "revision": "c7132470e050c49824e3d23141366b6f",
+    "url": "/swe363-project-react/static/media/football.c7132470.jfif"
+  }
+]);
